51721e0984d3c93dc5cba9603070062402cc2b6b
jwm0325 <jwm0325@gmail.com>
2024-10-24 22:01:23 +0900

    1

---
+++ vision/vision.js
@@ -0,0 +1,45 @@
+VISION_API_KEY = "AIzaSyCGBiHfELd3Y7wT6AhWrjimSngEUW27jLE"
+
+function processFile(event){
+    content = event.target.result 
+    imagestring = content.replace('data:image/jpeg;base64,', '')
+    document.getElementById("gimage").src = content
+}
+
+function uploadFiles(files){
+    file = files[0]
+    reader = new FileReader()
+    reader.onloadend = processFile
+    reader.readAsDataURL(file)
+}
+
+function analyze(){
+    data ={
+        requests: [{
+            image:{
+                content: imagestring
+            },
+            features:[{
+                type:"FACE_DETECTION",
+                maxResults: 100
+            }]
+        }]
+    }
+
+    $.ajax({
+        type:"POST",
+        url:'https://vision.googleapis.com/v1/images:annotate?key=' + VISION_API_KEY,
+        headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8"
+    }).done( function(response){
+        console.log(response)
+
+    }).fail(function(error){
+        console.log(error)
+
+    })
+}
